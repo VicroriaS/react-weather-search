b71a080fffb164162473ada9ed2451bd24ff073c
VicroriaS <120009279+VicroriaS@users.noreply.github.com>
2023-05-20 17:58:00 +0300

    Added github url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather-app
+      <h1>Weather-app</h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/frontenddevviktoriiasandul/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Viktoriia Sandul
+        </a>{" "}
+        is {""}
+        <a
+          href="https://github.com/VicroriaS/react-weather-search"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-soursed on Github
+        </a>
+      </footer>
     </div>
   );
 }
