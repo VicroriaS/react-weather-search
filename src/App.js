8959727880368f5916d21d32dd0c8f7ce640b758
VicroriaS <120009279+VicroriaS@users.noreply.github.com>
2023-05-21 11:17:21 +0300

    Added functions and react animated weather

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import Weather from './Weather';
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-      <Weather/>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/frontenddevviktoriiasandul/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Viktoriia Sandul
-        </a>{" "}
-        is {""}
-        <a
-          href="https://github.com/VicroriaS/react-weather-search"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-soursed on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="London" />
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/frontenddevviktoriiasandul/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Viktoriia Sandul
+          </a>{" "}
+          is {""}
+          <a
+            href="https://github.com/VicroriaS/react-weather-search"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-soursed on Github
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,74 @@
-import React, { useState } from "react";
-import "./Weather.css";
-import axios from "axios";
-export default function Weather(){
 
-   return (
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
       <div className="Weather">
-        <form>
-            <div className="row">
-          <div className="col-9">
-          <input
-            type="search"
-            placeholder="Type a city..."
-            className="form-control"
-            autoFocus="on"
-          />
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-        <div className="col-3">
-        <input type="submit" value="Search" class="btn btn-primary w-100" />
-        </div>
-        </div>
         </form>
-        <h1>Odessa</h1>
-        <ul>
-          <li>Wednesday 12:00</li>
-          <li>Sunny</li>
-        </ul>
-        <div className="container">
-          <div className="row mt-5">
-            <div className="col-6">
-    <img
-                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                alt="Sunny" />
-       
-     <span className="temperature">6</span>
-            <span className="unit">°</span>
-    
-            </div>
-            <div className="col-6">
-                  <ul>
-                <li>Precipitation</li>
-                <li>Humidity</li>
-                <li>Wind 10 km/h</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
+
+  
